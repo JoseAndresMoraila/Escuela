1b75b1451d559ec4992fdc61a2da331adaa97791
JoseAndresMoraila <141693744+JoseAndresMoraila@users.noreply.github.com>
2023-11-09 19:02:26 -0700

    Archivo principal
    Este es el archivo que se va a ejecutar y necesita 'DadoVariableScope.java' para que funcione

---
+++ Videos/Java/BroCode/ObjetosVariablesScope/VariableScope.java
@@ -0,0 +1,11 @@
+public class VariableScope { //EL ARCHIVO 'DadoVariableScope.java' ES IMPORTANTE PARA QUE ESTE FUNCIONE
+    public static void main(String[] args){
+        /* Vamos a explicar sobre las variables locales y globales dentro de una clase:
+         Variable local: Es declarada dentro de un metodo en una clase y solo puede ser visible y usada dentro del metodo
+
+         Variable global: Se declara afuera del método pero aún dentro de la clase y es visible tanto en todos los metodos de la clase
+                          como en general todas las partes dentro de la misma clase donde fue generada. */
+
+        DadoVariableScope Jugar = new DadoVariableScope();
+    }
+}
